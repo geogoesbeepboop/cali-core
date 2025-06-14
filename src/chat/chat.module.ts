8e3b2c7bfd522d248fe7efa82a7e2f185897b200
geogoesbeepboop <georgeandrade93@gmail.com>
2025-06-14 19:26:10 -0400

    changes to file organization

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
 import { FredModule } from '../fred/fred.module';
-import { LlmModule } from 'src/llm/llm.modulle';
+import { LlmModule } from 'src/llm/llm.module';
 
 @Module({
   imports: [FredModule, LlmModule],
